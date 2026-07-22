43d355b3666e21f6963af28763d17470a42bae78
Shady Abdallah <shadyabdallah241@gmail.com>
2026-07-22 08:45:41 +0300

    feat: add product flavors and environment configuration for dev and prod

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,8 +2,13 @@ import 'package:docdoc/core/di/dependency_injection.dart';
 import 'package:docdoc/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'doc_app.dart';
+import 'app_config.dart';
 
 void main() {
+  AppConfig.instantiate(
+    flavor: Flavor.dev,
+    apiBaseUrl: 'https://api-dev.example.com',
+  );
   setupGetIt();
   runApp(DocApp(appRouter: AppRouter()));
 }
